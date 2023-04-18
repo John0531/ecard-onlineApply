a170b6ab84900db97e2488a57c533834b061d2e3
shengzhang <shengzhang@uitc.com.tw>
2023-04-18 17:06:18 +0800

    footer修正

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,38 +1,65 @@
 <template>
   <div class="footer">
     <div class="serviceItems">
-        <div class="container-fluid">
-            <div class="contact">服務專線：<a href="tel:+886-2-25455168">(02)2545-5168</a>、<a href="tel:+886-7-226-9393">(07)226-9393</a>、拒絕接受行銷專線<a href="tel:+886-800-066-678">0800-066-678</a>、<a href="https://web.ubot.com.tw/call/notice_ecard202101.html" target="_blank">線上客服</a></div>
-            <div class="links">
-                <a href="index.htm">回首頁</a>
-                <a href="https://www.ubot.com.tw/" target="_blank">關於聯邦</a>
-                <a href="http://61.219.193.132/eCard/activity/2020newHomepage_6/sendMsg.htm" target="_blank">留言板</a>
-                <a href="https://www.ubot.com.tw/service_location" target="_blank">服務據點</a>
-                <a href="Announcement.htm">重要訊息公告</a>
-                <a href="CreditCard_info.htm">信用卡金融資訊</a>
-                <a href="https://web.ubot.com.tw/call/notice_ecard.html" target="_blank">無障礙服務專區</a>
-                <a href="Foreigner.htm">外籍人士專區 Foreigner Area</a>
-                <a href="Map.htm">網站導覽</a>
-                <a href="https://www.facebook.com/unionbankoftaiwan" target="_blank"><i class="fab fa-facebook-square"></i> 聯邦粉絲團</a>
-                <a href="https://liff.line.me/1645278921-kWRPP32q?accountId=ubot" target="_blank"><i class="fab fa-line"></i> LINE官方帳號</a>
-            </div>
+      <div class="container-fluid">
+        <div class="contact">
+          服務專線：<a href="tel:+886-2-25455168">(02)2545-5168</a>、<a
+            href="tel:+886-7-226-9393"
+            >(07)226-9393</a
+          >、拒絕接受行銷專線<a href="tel:+886-800-066-678">0800-066-678</a>、<a
+            href="https://web.ubot.com.tw/call/notice_ecard202101.html"
+            target="_blank"
+            >線上客服</a
+          >
         </div>
+        <div class="links">
+          <a href="index.htm">回首頁</a>
+          <a href="https://www.ubot.com.tw/" target="_blank">關於聯邦</a>
+          <a
+            href="http://61.219.193.132/eCard/activity/2020newHomepage_6/sendMsg.htm"
+            target="_blank"
+            >留言板</a
+          >
+          <a href="https://www.ubot.com.tw/service_location" target="_blank"
+            >服務據點</a
+          >
+          <a href="Announcement.htm">重要訊息公告</a>
+          <a href="CreditCard_info.htm">信用卡金融資訊</a>
+          <a
+            href="https://web.ubot.com.tw/call/notice_ecard.html"
+            target="_blank"
+            >無障礙服務專區</a
+          >
+          <a href="Foreigner.htm">外籍人士專區 Foreigner Area</a>
+          <a href="Map.htm">網站導覽</a>
+          <a href="https://www.facebook.com/unionbankoftaiwan" target="_blank"
+            ><i class="fab fa-facebook-square"></i>聯邦粉絲團</a
+          >
+          <a
+            href="https://liff.line.me/1645278921-kWRPP32q?accountId=ubot"
+            target="_blank"
+            ><i class="fab fa-line"></i>LINE官方帳號</a
+          >
+        </div>
+      </div>
     </div>
     <div class="information">
-        <div class="container-fluid">
-            <div class="row info">
-                <div class="infoLeft col-12 col-md-4 offset-md-2">謹慎理財 信用至上</div>
-                <div class="infoRight col-12 col-md-6 ">
-                    <ul class="text-left">
-                        <li>循環利率：5%~15%</li>
-                        <li>循環利率之基準日為104年9月1日</li>
-                        <li>預借現金手續費：預借金額x3.5%+100元</li>
-                        <li>其他費用請至卡友服務(各項費用計費說明)查詢</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="copyright">&copy; 聯邦銀行 All Rights Reserved.</div>
+      <div class="container-fluid">
+        <div class="row info">
+          <div class="infoLeft col-12 col-md-4 offset-md-2">
+            謹慎理財 信用至上
+          </div>
+          <div class="infoRight col-12 col-md-6">
+            <ul class="text-left">
+              <li>循環利率：5%~15%</li>
+              <li>循環利率之基準日為104年9月1日</li>
+              <li>預借現金手續費：預借金額x3.5%+100元</li>
+              <li>其他費用請至卡友服務(各項費用計費說明)查詢</li>
+            </ul>
+          </div>
         </div>
+        <div class="copyright">&copy; 聯邦銀行 All Rights Reserved.</div>
+      </div>
     </div>
   </div>
 </template>
