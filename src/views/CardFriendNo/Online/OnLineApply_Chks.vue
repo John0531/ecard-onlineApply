91bb0491f30eba330009675f3fb617fe1cebb5e7
林晏聖~ <robinlin@uitc.com.tw>
2023-05-02 11:49:10 +0800

    修改css

--- src/views/CardFriendNo/Online/OnLineApply_Chks.vue
+++ src/views/CardFriendNo/Online/OnLineApply_Chks.vue
@@ -77,17 +77,13 @@
                     rules="required"
                     />
                   <label for="checkbox1">
-                    <div  class="d-flex flex-wrap">
-                      <div class="flex-shrink-0">同意，本人對</div>
-                        <div class="" >
+                      同意，本人對
                           <template v-for="terms in termsFile" :key="terms+1">
                             「<a href="#" @click.prevent="checkAgreement" >
                               <u>{{terms}}</u>
                             </a>」
                           </template>
-                        </div>
-                      <div class="flex-shrink-0">內容。(請務必勾選)</div>
-                    </div>
+                      內容。(請務必勾選)
                   </label>
                   <div v-if="errors['服務申請約定條款']" class="d-flex text-center  field-error my-1" >
                   {{errors['服務申請約定條款']}}
