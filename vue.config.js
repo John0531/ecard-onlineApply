06a21cc1e5ff0a09488aaf920583f7b005a4f675
shengzhang <shengzhang@uitc.com.tw>
2023-07-05 08:53:38 +0800

    禁用代碼分割

--- vue.config.js
+++ vue.config.js
@@ -25,17 +25,18 @@ module.exports = {
   chainWebpack: config => {
     // 生產版本
     if (process.env.NODE_ENV === 'production') {
-      config.optimization.delete('splitChunks'); // 禁用代碼分割
+      config.optimization.delete('splitChunks') // 禁用代碼分割
 
       config.optimization.minimizer('terser').tap(args => {
         args[0].terserOptions = {
           ...args[0].terserOptions,
           output: {
             comments: false // 隱藏註解
-          },
-        };
+          }
+        }
         return args
-      });
+      })
     }
   },
+  productionSourceMap: process.env.NODE_ENV !== 'production'
 }
